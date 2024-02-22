fb417e86064a0da63eb4eab90a2a74d5c7a9882d
ashMatch <bruno.rochah95@gmail.com>
2024-02-22 00:57:50 -0300

    add atribute departament to the seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,14 +14,14 @@ public class Program extends RuntimeException{
 		Locale.setDefault(Locale.US);
 		
 		
-		Department dp = new Department(3404, "Caio Alves");
+		Department dp = new Department(3404, "Library");
 		
 		System.out.println(dp);
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Date date = sdf.parse("20/02/2022");
 		
-		Seller sl = new Seller(2345, "Sueli Montana", "sueli@gmail.com", new java.sql.Date(date.getTime()),3000.0);
+		Seller sl = new Seller(2345, "Sueli Montana", "sueli@gmail.com", new java.sql.Date(date.getTime()),3000.0, dp);
 		
 		System.out.println(sl);
 
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -14,17 +14,21 @@ public class Seller implements Serializable {
 	private Date birthDate;
 	private Double baseSalary;
 	
+	private Department department;
+	
+	
 	public Seller() {
 		
 	}
 	
-	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
+		this.department = department;
 	}
 
 	public Integer getId() {
@@ -66,6 +70,15 @@ public class Seller implements Serializable {
 	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
+	
+	public Department getDepartment() {
+		return department;
+	}
+	
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -95,7 +108,9 @@ public class Seller implements Serializable {
 				+ ", birthDate: " 
 				+ birthDate 
 				+ ", baseSalary: "
-				+ String.format("%.2f", baseSalary) 
+				+ String.format("%.2f", baseSalary)
+				+", department: "
+				+department
 				+ "]";
 	}
 	
