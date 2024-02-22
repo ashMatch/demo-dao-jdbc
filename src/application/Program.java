5e31ef7e2474cc1de5a9fb9a50d9cef44a6e266d
ashMatch <bruno.rochah95@gmail.com>
2024-02-22 16:39:40 -0300

    Implement the findById() class in SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,33 +1,38 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
-public class Program extends RuntimeException{
+public class Program {
 
-	public static void main(String[] args) throws ParseException {
-		Locale.setDefault(Locale.US);
+	public static void main(String[] args) {
+		//Locale.setDefault(Locale.US);
 		
 		
-		Department dp = new Department(3404, "Library");
+		/*
+		 * Department dp = new Department(3404, "Library");
+		 * 
+		 * System.out.println(dp);
+		 * 
+		 * SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); Date date =
+		 * sdf.parse("20/02/2022");
+		 * 
+		 * Seller sl = new Seller(2345, "Sueli Montana", "sueli@gmail.com", new
+		 * java.sql.Date(date.getTime()),3000.0, dp);
+		 * 
+		 * System.out.println(sl);
+		 */
 		
-		System.out.println(dp);
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Date date = sdf.parse("20/02/2022");
-		
-		Seller sl = new Seller(2345, "Sueli Montana", "sueli@gmail.com", new java.sql.Date(date.getTime()),3000.0, dp);
-		
-		System.out.println(sl);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+	
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
+	
 	}
 
 }
