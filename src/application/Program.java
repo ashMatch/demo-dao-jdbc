60a619361059922c30e9751ef23efbd1703a9f88
ashMatch <bruno.rochah95@gmail.com>
2024-02-22 22:18:10 -0300

    Implement findAll() method in the SellerDaoJDBC class

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,7 +38,13 @@ public class Program {
 		System.out.println("\n=== TEST n2: seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> listSeller = sellerDao.findByDepartment(department);
+		for (Seller sel : listSeller) {
+			System.out.println(sel);
+		}
 		
+		
+		System.out.println("\n=== TEST n3: seller findAll ===");
+		listSeller = sellerDao.findAll();
 		for (Seller sel : listSeller) {
 			System.out.println(sel);
 		}
